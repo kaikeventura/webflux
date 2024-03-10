730af8afbab8400ff595074d8e6c95e745aa3be5
kaikeventura <kaike.gnr@hotmail.com>
2024-03-09 21:25:48 -0300

    first commit

---
+++ src/test/kotlin/com/kaikeventura/webflux/WebfluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kaikeventura.webflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
